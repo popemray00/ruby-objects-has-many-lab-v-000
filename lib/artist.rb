d372b5b177360206f51e2e9dff5cc5f689f4c1d0
Michael Pope <popemray1986@gmail.com>
2017-11-28 22:50:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,16 @@
-def Artist
+class Artist
   attr_accessor :name
 
-  def new(name)
+  def initialize(name)
     @name = name
+    @songs = []
   end
 
-  def initialize
-    new
-
+  def add_song(song)
+    @songs << song
   end
 
+  def songs
+    @songs
+  end
 end
